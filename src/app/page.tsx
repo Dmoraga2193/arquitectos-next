e0d8dcce7e154ac4e8c7495312b51a06c1a41b4a
Dmoraga2193 <d.moraga@duocuc.cl>
2024-10-08 15:40:53 -0300

    mejoras en la UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ import {
   House,
   MapPin,
   Mail,
+  Clock,
 } from "lucide-react";
 import { useLoadScript, Autocomplete } from "@react-google-maps/api";
 import { Input } from "@/components/ui/input";
@@ -349,6 +350,31 @@ export default function LeyDelMonoPage() {
     }
   };
 
+  const services = [
+    {
+      icon: <CheckSquare className="h-8 w-8 text-blue-600" />,
+      title: "Evaluación Integral",
+      description: "Evaluamos y regularizamos todo tipo de construcciones",
+    },
+    {
+      icon: <FileText className="h-8 w-8 text-blue-600" />,
+      title: "Gestión de Trámites",
+      description:
+        "Gestionamos todos los trámites necesarios ante las autoridades competentes",
+    },
+    {
+      icon: <Users className="h-8 w-8 text-blue-600" />,
+      title: "Asesoría Personalizada",
+      description: "Ofrecemos asesoría personalizada durante todo el proceso",
+    },
+    {
+      icon: <Shield className="h-8 w-8 text-blue-600" />,
+      title: "Cumplimiento Normativo",
+      description:
+        "Garantizamos el cumplimiento de todas las normativas vigentes",
+    },
+  ];
+
   const faqs = [
     {
       question: "¿Qué es la Ley del Mono?",
@@ -527,58 +553,38 @@ export default function LeyDelMonoPage() {
       {/* Contenido Principal */}
       <main className="container mx-auto px-4  max-w-4xl py-16">
         {/* Sección Conócenos */}
-        <Card id="conocenos" className="mb-12 scroll-mt-20" data-aos="zoom-in">
-          <CardHeader>
-            <CardTitle className="text-3xl font-semibold flex items-center text-blue-800">
-              <Users className="mr-2 h-8 w-8" /> Conócenos
+        <Card
+          id="conocenos"
+          className="mb-12 overflow-hidden shadow-lg"
+          data-aos="zoom-in"
+        >
+          <CardHeader className="bg-blue-800 text-white p-6">
+            <CardTitle className="text-3xl font-bold flex items-center">
+              <Users className="mr-3 h-8 w-8" /> Conócenos
             </CardTitle>
           </CardHeader>
-          <CardContent>
-            <p className="text-lg mb-6 text-gray-700">
+          <CardContent className="p-6">
+            <p className="text-lg mb-8 text-gray-700 leading-relaxed">
               Somos un grupo de arquitectos especializados en la regularización
               de proyectos bajo la Ley del Mono chilena. Nuestro objetivo es
               ayudar a propietarios a legalizar sus construcciones de manera
               eficiente y profesional.
             </p>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              <div className="flex items-start">
-                <CheckSquare className="h-6 w-6 text-blue-600 mr-2 mt-1" />
-                <div>
-                  <h3 className="font-semibold mb-2">Evaluación Integral</h3>
-                  <p className="text-gray-700">
-                    Evaluamos y regularizamos todo tipo de construcciones
-                  </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+              {services.map((service, index) => (
+                <div
+                  key={index}
+                  className="flex items-start bg-white p-4 rounded-lg shadow-md transition-all duration-300 hover:shadow-xl hover:-translate-y-1"
+                >
+                  <div className="flex-shrink-0 mr-4">{service.icon}</div>
+                  <div>
+                    <h3 className="font-semibold text-xl mb-2 text-blue-800">
+                      {service.title}
+                    </h3>
+                    <p className="text-gray-600">{service.description}</p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex items-start">
-                <FileText className="h-6 w-6 text-blue-600 mr-2 mt-1" />
-                <div>
-                  <h3 className="font-semibold mb-2">Gestión de Trámites</h3>
-                  <p className="text-gray-700">
-                    Gestionamos todos los trámites necesarios ante las
-                    autoridades competentes
-                  </p>
-                </div>
-              </div>
-              <div className="flex items-start">
-                <Users className="h-6 w-6 text-blue-600 mr-2 mt-1" />
-                <div>
-                  <h3 className="font-semibold mb-2">Asesoría Personalizada</h3>
-                  <p className="text-gray-700">
-                    Ofrecemos asesoría personalizada durante todo el proceso
-                  </p>
-                </div>
-              </div>
-              <div className="flex items-start">
-                <Shield className="h-6 w-6 text-blue-600 mr-2 mt-1" />
-                <div>
-                  <h3 className="font-semibold mb-2">Cumplimiento Normativo</h3>
-                  <p className="text-gray-700">
-                    Garantizamos el cumplimiento de todas las normativas
-                    vigentes
-                  </p>
-                </div>
-              </div>
+              ))}
             </div>
           </CardContent>
         </Card>
@@ -1308,62 +1314,74 @@ export default function LeyDelMonoPage() {
         </Card>
 
         {/* Nueva sección: Nuestra Sucursal */}
-        <section id="contacto" className="mb-12 mt-12 scroll-mt-20 py-18">
-          <h2
-            className="text-3xl font-semibold text-blue-800 text-center mb-8"
-            data-aos="zoom-in"
-          >
-            Nuestra Sucursal
-          </h2>
-          <Card className="mb-6" data-aos="fade-up">
-            <CardContent className="p-6">
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                <div>
-                  <h3 className="text-xl font-semibold mb-4 text-blue-800">
-                    Información de Contacto
-                  </h3>
-                  <div className="space-y-2">
-                    <p className="flex items-center">
-                      <MapPin className="h-10 w-10 text-blue-600 mr-2" />
-                      Radal del Obispo Francisco Anabalón Duarte 1015, 8500771
-                      Quinta Normal, Región Metropolitana
-                    </p>
-                    <p className="flex items-center">
-                      <Phone className="h-5 w-5 text-blue-600 mr-2" />
-                      +56 2 2345 6789
-                    </p>
-                    <p className="flex items-center">
-                      <Mail className="h-5 w-5 text-blue-600 mr-2" />
-                      contacto@arquitectosnext.cl
-                    </p>
+        <section id="contacto" className="py-20">
+          <div className="container mx-auto px-4">
+            <h2 className="text-4xl font-bold text-center text-blue-800 mb-12">
+              Nuestra Sucursal
+            </h2>
+            <Card className="overflow-hidden shadow-lg">
+              <CardContent className="p-0">
+                <div className="grid grid-cols-1 md:grid-cols-2">
+                  <div className="p-8 bg-blue-800 text-white">
+                    <h3 className="text-2xl font-semibold mb-6">
+                      Información de Contacto
+                    </h3>
+                    <div className="space-y-4">
+                      <p className="flex items-start">
+                        <MapPin className="h-6 w-6 mr-4 mt-1 flex-shrink-0" />
+                        <span>
+                          Radal del Obispo Francisco Anabalón Duarte 1015,
+                          8500771 Quinta Normal, Región Metropolitana
+                        </span>
+                      </p>
+                      <p className="flex items-center">
+                        <Phone className="h-6 w-6 mr-4 flex-shrink-0" />
+                        <span>+56 2 2345 6789</span>
+                      </p>
+                      <p className="flex items-center">
+                        <Mail className="h-6 w-6 mr-4 flex-shrink-0" />
+                        <span>contacto@arquitectosnext.cl</span>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="p-8 bg-white">
+                    <h3 className="text-2xl font-semibold mb-6 text-blue-800">
+                      Horario de Atención
+                    </h3>
+                    <div className="space-y-2">
+                      <p className="flex items-center">
+                        <Clock className="h-5 w-5 mr-3 text-blue-600" />
+                        <span>Lunes a Jueves: 8:30 AM - 6:00 PM</span>
+                      </p>
+                      <p className="flex items-center">
+                        <Clock className="h-5 w-5 mr-3 text-blue-600" />
+                        <span>Viernes: 8:30 AM - 4:45 PM</span>
+                      </p>
+                      <p className="flex items-center">
+                        <Clock className="h-5 w-5 mr-3 text-blue-600" />
+                        <span>Sábado y Domingo: Cerrado</span>
+                      </p>
+                    </div>
                   </div>
                 </div>
-                <div>
-                  <h3 className="text-xl font-semibold mb-4 text-blue-800">
-                    Horario de Atención
-                  </h3>
-                  <p>Lunes a Jueves: 8:30 AM - 6:00 PM</p>
-                  <p>Viernes: 8:30 AM - 4:45 PM</p>
-                  <p>Sabado y Domingo: Cerrado</p>
+              </CardContent>
+            </Card>
+            <div className="relative mt-20" data-aos="zoom-in">
+              <div className="absolute inset-0 bg-blue-200 transform rotate-3 rounded-lg shadow-lg"></div>
+              <div className="absolute inset-0 bg-white transform -rotate-3 rounded-lg shadow-lg"></div>
+              <div className="relative bg-white p-4 rounded-lg shadow-xl transform transition-all duration-300 hover:scale-105">
+                <div className="aspect-w-16 aspect-h-9">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d699.8964686150196!2d-70.7027135670081!3d-33.441817032144115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c46fa6a860c7%3A0xb4d5edc02fce12df!2sRadal%20del%20Obispo%20Francisco%20Anabal%C3%B3n%20Duarte%201015%2C%208500771%20Quinta%20Normal%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1728328131503!5m2!1ses-419!2scl"
+                    width="100%"
+                    height="450"
+                    style={{ border: 0 }}
+                    allowFullScreen
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    title="Ubicación de nuestra sucursal"
+                  ></iframe>
                 </div>
-              </div>
-            </CardContent>
-          </Card>
-          <div className="relative mt-8" data-aos="zoom-in">
-            <div className="absolute inset-0 bg-blue-200 transform rotate-3 rounded-lg shadow-lg"></div>
-            <div className="absolute inset-0 bg-white transform -rotate-3 rounded-lg shadow-lg"></div>
-            <div className="relative bg-white p-4 rounded-lg shadow-xl transform transition-all duration-300 hover:scale-105">
-              <div className="aspect-w-16 aspect-h-9">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d699.8964686150196!2d-70.7027135670081!3d-33.441817032144115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c46fa6a860c7%3A0xb4d5edc02fce12df!2sRadal%20del%20Obispo%20Francisco%20Anabal%C3%B3n%20Duarte%201015%2C%208500771%20Quinta%20Normal%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1728328131503!5m2!1ses-419!2scl"
-                  width="100%"
-                  height="450"
-                  style={{ border: 0 }}
-                  allowFullScreen
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-                  title="Ubicación de nuestra sucursal"
-                ></iframe>
               </div>
             </div>
           </div>
